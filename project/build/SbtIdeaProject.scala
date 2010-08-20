f62ad4e1de275d1612a850211723fb5ccf815a93
odd <odd@traveas.com>
2010-08-20 09:32:31 +0200

    Removed plugin IdeaPlugin.

--- project/build/SbtIdeaProject.scala
+++ project/build/SbtIdeaProject.scala
@@ -5,8 +5,8 @@ class SbtIdeaProject(info:ProjectInfo) extends ParentProject(info) {
   lazy val publishTo = Resolver.file("GitHub Pages", new java.io.File("../mpeltonen.github.com/maven/"))
 
   lazy val core = project("sbt-idea-core", "sbt-idea-core", new Core(_))
-  lazy val plugin = project("sbt-idea-plugin", "sbt-idea-plugin", new PluginProject(_) with IdeaPlugin, core)
-  lazy val processor = project("sbt-idea-processor", "sbt-idea-processor", new ProcessorProject(_) with IdeaPlugin, core)
+  lazy val plugin = project("sbt-idea-plugin", "sbt-idea-plugin", new PluginProject(_), core)
+  lazy val processor = project("sbt-idea-processor", "sbt-idea-processor", new ProcessorProject(_), core)
 
   class Core(info:ProjectInfo) extends DefaultProject(info) {
     override def unmanagedClasspath = super.unmanagedClasspath +++ info.sbtClasspath
